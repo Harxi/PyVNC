04ec5a0e24c246c79306cd2b7e817ab2db8a1ab2
Harxi <90150354+Harxi@users.noreply.github.com>
2023-05-29 15:30:53 +0300

    Update utilities.py

--- src/utilities.py
+++ src/utilities.py
@@ -1,13 +1,11 @@
 import struct
 
 def bytesToInteger(b: bytes) -> int:
-
     return struct.unpack(">I", b)[0]
 
     
 
 def bytesToShort(b: bytes) -> int:
-
     return struct.unpack(">H", b)[0]
 
         
